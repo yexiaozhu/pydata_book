bccc40d2464f602d5eb73653da8657fb8e0431dd
557703988@139.com <557703988@139.com>
2017-06-22 23:14:03 -0700

    Chapter 09: 数据聚合与分组运算

--- analyse.py
+++ analyse.py
@@ -1,216 +1,245 @@
 #!/usr/bin/env python 2.7.12
 #coding=utf-8
 #author=yexiaozhu
-
-import matplotlib.pyplot as plt
-
-# fig = plt.figure()
-# ax1 = fig.add_subplot(2, 2, 1)
-# ax2 = fig.add_subplot(2, 2, 2)
-# ax3 = fig.add_subplot(2, 2, 3)
 import pandas as pd
-from numpy.random import randn, np
-# plt.plot(randn(50).cumsum(), 'k--')
-# _ = ax1.hist(randn(100), bins=20, color='k', alpha=0.3)
-# ax2.scatter(np.arange(30), np.arange(30) + 3 * randn(30))
-# fig, axes = plt.subplots(2, 3)
-# print axes
-# subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=None)
-# plt.show()
-# fig, axes = plt.subplots(2, 2, sharex=True, sharey=True)
-# for i in range(2):
-    # for j in range(2):
-        # axes[i, j].hist(randn(500), bins=50, color='k', alpha=0.5)
-# plt.subplots_adjust(wspace=0, hspace=0)
-# plt.show()
-# plt.plot(randn(30).cumsum(), 'ko--')
-# plt.plot(randn(30).cumsum(), color='k', linestyle='dashed', marker='o')
-# plt.show()
-data = randn(30).cumsum()
-# plt.plot(data, 'k--', label='Default')
-# plt.plot(data, 'k-', drawstyle='steps-post', label='steps-post')
-# plt.legend(loc='best')
-# plt.show()
-# fig = plt.figure(); ax = fig.add_subplot(1, 1, 1)
-# ax.plot(randn(1000).cumsum())
-# plt.show()
-# ticks = ax.set_xticks([0, 250, 500, 750, 1000])
-# labels = ax.set_xticklabels(['one', 'two', 'three', 'four', 'five'],
-#                             rotation=30, fontsize='small')
-# ax.set_title('My first matplotlib plot')
-# ax.set_xlabel('Stages')
-# ax.plot(randn(1000).cumsum(), 'k', label='one')
-# ax.plot(randn(1000).cumsum(), 'k--', label='two')
-# ax.plot(randn(1000).cumsum(), 'k.', label='three')
-# ax.legend(loc='best')
-# plt.show()
-# ax.text(x, y, 'Hello world!', family='monospace', fontsize=10)
-from datetime import datetime
-# fig = plt.figure()
-# ax = fig.add_subplot(1, 1, 1)
-date = pd.read_csv('ch08/spx.csv', index_col=0, parse_dates=True)
-spx = date['SPX']
-# spx.plot(ax=ax, style='k-')
-crisis_data = [
-    (datetime(2007, 10, 11), 'Peak of bull market'),
-    (datetime(2008, 3, 12), 'Bear Stearns Fails'),
-    (datetime(2008, 9, 15), 'Lehman Bankruptcy'),
-]
-# for date, label in crisis_data:
-#     # print date
-#     # print label
-#     # print spx.asof(date)
-#     # print type(ax)
-#     ax.annotate(label, xy=(date, spx.asof(date) + 50),
-#                 xytext=(date, spx.asof(date) + 200),
-#                 arrowprops=dict(facecolor='black'),
-#                 horizontalalignment='left', verticalalignment='top')
-# ax.set_xlim(['1/1/2007', '1/1/2011'])
-# ax.set_ylim([600, 1800])
-# ax.set_title('Important dates in 2008-2009 financial crisis')
-# plt.show()
-# fig = plt.figure()
-# ax = fig.add_subplot(1, 1, 1)
-# rect = plt.Rectangle((0.2, 0.75), 0.4, 0.15, color='k', alpha=0.3)
-# circ = plt.Circle((0.7, 0.2), 0.15, color='b', alpha=0.3)
-# pgon = plt.Polygon([[0.15, 0.15], [0.35, 0.4], [0.2, 0.6]], color='g', alpha=0.5)
-# ax.add_patch(rect)
-# ax.add_patch(circ)
-# ax.add_patch(pgon)
-# plt.show()
-# plt.savefig('figpath.png', dpi=400)
-# plt.savefig('figpath1.png', dpi=400, bbox_inches='tight')
-# from io import StringIO
-# buffer = StringIO()
-# plt.savefig(buffer)
-# plot_data = buffer.getvalue()
-from pandas import Series, DataFrame
-s = Series(np.random.randn(10).cumsum(), index=np.arange(0, 100, 10))
-# s.plot()
-# plt.show()
-df = DataFrame(np.random.randn(10, 4).cumsum(0),
-               columns=['A', 'B', 'C', 'D'],
-               index=np.arange(0, 100, 10))
-# df.plot()
-# plt.show()
-# fig, axes = plt.subplots(2, 1)
-date = Series(np.random.rand(16), index=list('abcdefghijklmnop'))
-# date.plot(kind='bar', ax=axes[0], color='k', alpha=0.7)
-# date.plot(kind='barh', ax=axes[1], color='k', alpha=0.7)
-df = DataFrame(np.random.rand(6, 4),
-               index=['one', 'two', 'three', 'four', 'five', 'six'],
-               columns=pd.Index(['A', 'B', 'C', 'D'], name='Genus'))
+from pandas import DataFrame, np, Series
+
+df = DataFrame({'key1' : ['a', 'a', 'b', 'b', 'a'],
+                'key2' : ['one', 'two', 'one', 'two', 'one'],
+                'data1' : np.random.randn(5),
+                'data2' : np.random.randn(5)})
 # print df
-# df.plot(kind='bar')
-# df.plot(kind='barh', stacked=True, alpha=0.5)
-# plt.show()
+grouped = df['data1'].groupby(df['key1'])
+# print grouped
+# print grouped.mean()
+means = df['data1'].groupby([df['key1'], df['key2']]).mean()
+# print means
+# print means.unstack()
+states = np.array(['Ohio', 'California', 'California', 'Ohio', 'Ohio'])
+years = np.array([2005, 2005, 2006, 2005, 2006])
+# print df['data1'].groupby([states, years]).mean()
+# print df.groupby('key1').mean()
+# print df.groupby(['key1', 'key2']).mean()
+# print df.groupby(['key1', 'key2']).size()
+# for name, group in df.groupby('key1'):
+#     print name
+#     print group
+# for (k1, k2), group in df.groupby(['key1', 'key2']):
+#     print k1, k2
+#     print group
+pieces = dict(list(df.groupby('key1')))
+# print pieces['b']
+# print df.dtypes
+grouped = df.groupby(df.dtypes, axis=1)
+# print dict(list(grouped))
+# print df.groupby(['key1', 'key2'])[['data2']].mean()
+s_grouped = df.groupby(['key1', 'key2'])['data2']
+# print s_grouped
+# print s_grouped.mean()
+people = DataFrame(np.random.randn(5, 5),
+                   columns=['a', 'b', 'c', 'd', 'e'],
+                   index=['Joe', 'Steve', 'Wes', 'Jim', 'Travis'])
+people.ix[2:3, ['b', 'c']] = np.nan
+# print people
+mapping = {'a': 'red', 'b': 'red', 'c': 'blue',
+           'd': 'blue', 'e': 'red', 'f': 'orange'}
+by_column = people.groupby(mapping, axis=1)
+# print by_column.sum()
+map_series = Series(mapping)
+# print map_series
+# print people.groupby(map_series, axis=1).count()
+# print people.groupby(len).sum()
+key_list = ['one', 'one', 'one', 'two', 'two']
+# print people.groupby([len, key_list]).min()
+columns = pd.MultiIndex.from_arrays([['US', 'US', 'US', 'JP', 'JP'],
+                                     [1, 3, 5, 1, 3]], names=['cty', 'tenor'])
+hier_df = DataFrame(np.random.randn(4, 5), columns=columns)
+# print hier_df
+# print hier_df.groupby(level='cty', axis=1).count()
+# print df
+grouped = df.groupby('key1')
+# print grouped['data1'].quantile(0.9)
+def peak_to_peak(arr):
+    return arr.max() - arr.min()
+# print grouped.agg(peak_to_peak)
+# print grouped.describe()
 tips = pd.read_csv('ch08/tips.csv')
-# print tips.day
-party_counts = pd.crosstab(tips['day'], tips['size'])
-# print party_counts
-party_counts = party_counts.ix[:, 2:5]
-# print party_counts
-party_pcts = party_counts.div(party_counts.sum(1).astype(float), axis=0)
-# print party_pcts
-# party_pcts.plot(kind='bar', stacked=True)
-# plt.show()
 tips['tip_pct'] = tips['tip'] / tips['total_bill']
-# tips['tip_pct'].hist(bins=50)
-# tips['tip_pct'].plot(kind='kde')
-# plt.show()
-comp1 = np.random.normal(0, 1, size=200) # N(0, 1)
-comp2 = np.random.normal(10, 2, size=200) # N(10, 4)
-values = Series(np.concatenate([comp1, comp2]))
-# values.hist(bins=100, alpha=0.3, color='k', normed=True)
-# values.plot(kind='kde', style='k--')
-# plt.show()
-macro = pd.read_csv('ch08/macrodata.csv')
-data = macro[['cpi', 'm1', 'tbilrate', 'unemp']]
-# print date
-# print type(date)
-# print type(np.log(data))
-trans_data = np.log(data).diff().dropna()
-# print trans_data
-# print trans_data[-5:]
-# plt.scatter(trans_data['m1'], trans_data['unemp'])
-# plt.title('Changes in log %s vs. log %s' %('m1', 'unemp'))
-# pd.scatter_matrix(trans_data, diagonal='kde', color='k', alpha=0.3)
-# pd.plotting.scatter_matrix(trans_data, diagonal='kde', color='k', alpha=0.3)# scatter_matrix已被plotting.scatter_matrix替换
-# plt.show()
-data = pd.read_csv('ch08/Haiti.csv')
+# print tips[:6]
+grouped = tips.groupby(['sex', 'smoker'])
+grouped_pct = grouped['tip_pct']
+# print grouped_pct.agg('mean')
+# print grouped_pct.agg(['mean', 'std', peak_to_peak])
+# print grouped_pct.agg([('foo', 'mean'),( 'bar', np.std)])
+functions = ['count', 'mean', 'max']
+result = grouped['tip_pct', 'total_bill'].agg(functions)
+# print result
+# print result['tip_pct']
+ftuples = [('Durchschnitt', 'mean'), ('Abweichung', np.var)]
+# print grouped['tip_pct', 'total_bill'].agg(ftuples)
+# print grouped.agg({'tip': np.max, 'size' : 'sum'})
+# print grouped.agg({'tip': ['min', 'max', 'mean', 'std'], 'size' : 'sum'})
+# print tips.groupby(['sex', 'smoker'], as_index=False).mean()
+# print df
+k1_means = df.groupby('key1').mean().add_prefix('mean_')
+# print k1_means
+# print pd.merge(df, k1_means, left_on='key1', right_index=True)
+key = ['one', 'two', 'one', 'two', 'one']
+# print people.groupby(key).mean()
+# print people.groupby(key).transform(np.mean)
+def demean(arr):
+    return arr - arr.mean()
+demeaned = people.groupby(key).transform(demean)
+# print demeaned
+# print demeaned.groupby(key).mean()
+def top(df, n=5, column='tip_pct'):
+    return df.sort_values(by=column)[-n:]
+# print top(tips, n=6)
+# print tips.groupby('smoker').apply(top)
+# print tips.groupby(['smoker', 'day']).apply(top, n=1, column='total_bill')
+result = tips.groupby('smoker')['tip_pct'].describe()
+# print result
+# print result.unstack('smoker')
+# print tips.groupby('smoker', group_keys=False).apply(top)
+frame = DataFrame({'data1': np.random.randn(1000),
+                   'data2': np.random.randn(1000)})
+factor = pd.cut(frame.data1, 4)
+# print factor[:10]
+def get_stats(group):
+    return {'min': group.min(), 'max': group.max(),
+            'count': group.count, 'mean': group.mean()}
+grouped = frame.data2.groupby(factor)
+# print grouped.apply(get_stats).unstack()
+grouping = pd.qcut(frame.data1, 10, labels=False)
+grouped = frame.data2.groupby(grouping)
+# print grouped.apply(get_stats).unstack()
+states = ['Ohio', 'New York', 'Vermont', 'Florida',
+          'Oregon', 'Nevada', 'California', 'Idaho']
+group_key = ['East'] * 4 + ['West'] * 4
+data = Series(np.random.randn(8), index=states)
+data[['Vermont', 'Nevada', 'Idaho']] = np.nan
 # print data
-# print data[['INCIDENT DATE', 'LATITUDE', "LONGITUDE"]][:10]
-# print data['CATEGORY'][:6]
-# print data.describe()
-data = data[(data.LATITUDE > 18) & (data.LATITUDE < 20) & (data.LONGITUDE > -75)
-            & (data.LONGITUDE < -70) & data.CATEGORY.notnull()]
-# print data.describe()
-def to_cat_list(catstr):
-    stripped = (x.strip() for x in catstr.split(','))
-    return [x for x in stripped if x]
+# print data.groupby(group_key).mean()
+fill_mean = lambda g: g.fillna(g.mean())
+# print data.groupby(group_key).apply(fill_mean)
+fill_values = {'East': 0.5, 'West': -1}
+fill_func = lambda g: g.fillna(fill_values[g.name])
+# print data.groupby(group_key).apply(fill_func)
+# 红桃(Hearts), 黑桃(Spades), 梅花(Clubs), 方片(DIamonds)
+suits = ['H', 'S', 'C', 'D']
+card_val = (range(1, 11) + [10]* 3) * 4
+base_names = ['A'] + range(2, 11) + ['J', 'K', 'Q']
+cards = []
+for suit in ['H', 'S', 'C', 'D']:
+    cards.extend(str(num) + suit for num in base_names)
+deck = Series(card_val, index=cards)
+# print deck[:13]
+def draw(deck, n=5):
+    return deck.take(np.random.permutation(len(deck))[:n])
+# print draw(deck)
+get_suit = lambda card: card[-1]
+# print deck.groupby(get_suit).apply(draw, n=2)
+# print deck.groupby(get_suit, group_keys=False).apply(draw, n=2)
+df = DataFrame({'category': ['a', 'a', 'a', 'a', 'b', 'b', 'b', 'b'],
+                'data': np.random.randn(8),
+                'weights': np.random.rand(8)})
+# print df
+grouped = df.groupby('category')
+get_wavg = lambda g: np.average(g['data'], weights=g['weights'])
+# print grouped.apply(get_wavg)
+close_px = pd.read_csv('ch09/stock_px.csv', parse_dates=True, index_col=0)
+# print close_px
+# print close_px[-4:]
+rets = close_px.pct_change().dropna()
+spx_corr = lambda x: x.corrwith(x['SPX'])
+by_year = rets.groupby(lambda x: x.year)
+# print by_year.apply(spx_corr)
+# print by_year.apply(lambda g: g['AAPL'].corr(g['MSFT']))
+# import statsmodels.api as sm
+# def regress(data, yvar, xvars):
+#     Y = data[yvar]
+#     X = data[xvars]
+#     X['intercept'] = 1.
+#     result = sm.OLS(Y, X).fit()
+#     return result.params
+# print by_year.apply(regress, 'AAPL', ['SPX'])
+# print tips.pivot_table(['tip_pct', 'size'], index=['sex', 'day'],
+#                        columns='smoker', margins=True)
+# print tips.pivot_table('tip_pct', index=['sex', 'smoker'], columns='day', aggfunc=len, margins=True)
+# print tips.pivot_table('size', index=['time', 'sex', 'smoker'], columns='day', aggfunc='sum', fill_value=0)
+fec = pd.read_csv('ch09/P00000001-ALL.csv', low_memory=False)
+# print fec
+# print fec.ix[123456]
+unique_cands = fec.cand_nm.unique()
+# print unique_cands
+parties = {'Bachmann, Michelle': 'Republican',
+           'Romney, Mitt': 'Republican',
+           'Obama, Barack': 'Democrat',
+           "Roemer, Charles E. 'Buddy' III": 'Republican',
+           'Pawlenty, Timothy': 'Republican',
+           'Johnson, Gary Earl': 'Republican',
+           'Paul, Ron': 'Republican',
+           'Santorum, Rick': 'Republican',
+           'Cain, Herman': 'Republican',
+           'Gingrich, Newt': 'Republican',
+           'McCotter, Thaddeus G': 'Republican',
+           'Huntsman, Jon': 'Republican',
+           'Perry, Rick': 'Republican'}
+# print fec.cand_nm[123456:123461]
+# print fec.cand_nm[123456:123461].map(parties)
+fec['party'] = fec.cand_nm.map(parties)
+# print fec['party'].value_counts()
+# print (fec.contb_receipt_amt > 0).value_counts()
+fec = fec[fec.contb_receipt_amt > 0]
+fec_mrbo = fec[fec.cand_nm.isin(['Obama, Barack', 'Romney, Mitt'])]
+# print fec.contbr_occupation.value_counts()[:10]
+occ_mapping = {
+   'INFORMATION REQUESTED PER BEST EFFORTS' : 'NOT PROVIDED',
+   'INFORMATION REQUESTED' : 'NOT PROVIDED',
+   'INFORMATION REQUESTED (BEST EFFORTS)' : 'NOT PROVIDED',
+   'C.E.O.': 'CEO'
+}
+# If no mapping provided, return x
+f = lambda x: occ_mapping.get(x, x)
+fec.contbr_occupation = fec.contbr_occupation.map(f)
+emp_mapping = {
+   'INFORMATION REQUESTED PER BEST EFFORTS' : 'NOT PROVIDED',
+   'INFORMATION REQUESTED' : 'NOT PROVIDED',
+   'SELF' : 'SELF-EMPLOYED',
+   'SELF EMPLOYED' : 'SELF-EMPLOYED',
+}
 
-def get_all_categories(cat_series):
-    cat_sets = (set(to_cat_list(x)) for x in cat_series)
-    return sorted(set.union(*cat_sets))
-
-def get_english(cat):
-    code, names = cat.split('.')
-    if '|' in names:
-        names = names.split('|')[1]
-    return code, names.strip()
-
-# print get_english('2. Urgences logistiques | Vital Lines')
-all_cats = get_all_categories(data.CATEGORY)
-english_mapping = dict(get_english(x) for x in all_cats)
-# print english_mapping['2a']
-# print english_mapping['6c']
-def get_code(seq):
-    return [x.split('.')[0] for x in seq if x]
-all_codes = get_code(all_cats)
-code_index = pd.Index(np.unique(all_codes))
-dummy_frame = DataFrame(np.zeros((len(data), len(code_index))), index=data.index, columns=code_index)
-# print dummy_frame.ix[:, :6]
-for row, cat in zip(data.index, data.CATEGORY):
-    codes = get_code(to_cat_list(cat))
-    dummy_frame.ix[row, codes] = 1
-
-data = data.join(dummy_frame.add_prefix('category_'))
-# print data.ix[:, 10:15]
-from mpl_toolkits.basemap import Basemap
+# If no mapping provided, return x
+f = lambda x: emp_mapping.get(x, x)
+fec.contbr_employer = fec.contbr_employer.map(f)
+by_occupation = fec.pivot_table('contb_receipt_amt',
+                                index='contbr_occupation',
+                                columns='party', aggfunc='sum')
+over_2mm = by_occupation[by_occupation.sum(1) > 2000000]
+# print over_2mm
+# over_2mm.plot(kind='barh')
 import matplotlib.pyplot as plt
-def basic_haiti_map(ax=None, lllat=17.25, urlat=20.25, lllon=-75, urlon=-71):
-    # 创建极球面投影的Basemap实例
-    m = Basemap(ax=ax, projection='stere',
-                lon_0=(urlon + lllon) / 2,
-                lat_0=(urlat + lllat) / 2,
-                llcrnrlat=lllat, urcrnrlat=urlat,
-                llcrnrlon=lllon, urcrnrlon=urlon,
-                resolution='f')
-    # 绘制海岸线,州界,国界,地图边界
-    m.drawcoastlines()
-    m.drawstates()
-    m.drawcountries()
-    return m
-fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(12, 10))
-fig.subplots_adjust(hspace=0.05, wspace=0.05)
-to_plot = ['2a', '1', '3c', '7a']
-lllat = 17.25; urlat = 20.25; lllon = -75; urlon=-71
-# print data['category_2a']
-# print data[data['category_2a'] == 1]
-# print data[data['category_2a'] == 1].LONGITUDE
-for code, ax in zip(to_plot, axes.flat):
-    m = basic_haiti_map(ax, lllat=lllat, urlat=urlat, lllon=lllon, urlon=urlon)
-    # print m
-    cat_data = data[data['category_%s' % code] == 1]
-    # print cat_data.LONGITUDE
-
-    # 计算地图投影坐标
-    x, y = m(cat_data.LONGITUDE.values, cat_data.LATITUDE.values)
-    # This is resolved by changing m(cat_data.LONGITUDE, cat_data.LATITUDE) to m(cat_data.LONGITUDE.values, cat_data.LATITUDE.values)
-    # print x
-    # m.plot(x, y, 'k.', alpha=0.5)
-    ax.set_title('%s: %s' %(code, english_mapping[code]))
-    # plt.show()
 # plt.show()
-shapefile_path = 'ch08/PortAuPrince_Roads/PortAuPrince_Roads'
-m.readshapefile(shapefile_path, 'roads')
+def get_top_amounts(group, key, n=5):
+    totals = group.groupby(key)['contb_receipt_amt'].sum()
+    return totals.sort_values(ascending=False)[n:]
+grouped = fec_mrbo.groupby('cand_nm')
+# print grouped.apply(get_top_amounts, 'contbr_occupation', n=7)
+# print grouped.apply(get_top_amounts, 'contbr_employer', n=10)
+bins = np.array([0, 1, 10, 100, 1000, 10000, 100000, 1000000, 10000000])
+labels = pd.cut(fec_mrbo.contb_receipt_amt, bins)
+# print labels
+grouped = fec_mrbo.groupby(['cand_nm', labels])
+# print grouped.size().unstack(0)
+bucket_sums = grouped.contb_receipt_amt.sum().unstack(0)
+# print bucket_sums
+normed_sums = bucket_sums.div(bucket_sums.sum(axis=1), axis=0)
+# print normed_sums
+# normed_sums[:-2].plot(kind='barh', stacked=True)
+# plt.show()
+grouped = fec_mrbo.groupby(['cand_nm', 'contbr_st'])
+totals = grouped.contb_receipt_amt.sum().unstack(0).fillna(0)
+totals = totals[totals.sum(1) > 100000]
+# print totals[:10]
+percent = totals.div(totals.sum(1), axis=0)
+# print percent[:10]
